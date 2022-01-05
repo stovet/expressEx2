4f29c6019c9b6ed00ace47899671ae7310d3fcca
stovet <steven.topalian@gmail.com>
2022-01-05 15:19:43 -0500

    specific shops showing up

--- src/routes/shop-details-routes.ts
+++ src/routes/shop-details-routes.ts
@@ -11,26 +11,21 @@ const shops: Shop[] = [
     { id: 555, name: "Teddy's Tunes", rating: 4.7 }
 ];
 
-shopDetails.get("/", function(req,res){
-    res.render('shop-details', 
-    {
-       shops: [
-            {id: 111, name: "Pepper's Pizza", rating: 4.5},
-            {id: 222, name: "Clive's Chives", rating: 3.4},
-            {id: 333, name: "Bretty's Brews", rating: 4.3},
-            { id: 444, name: "Sylvester's Shoes", rating: 3.8 },
-            { id: 555, name: "Teddy's Tunes", rating: 4.7 }
-        ]
-    });
-})
-
+// shopDetails.get("/", function(req,res){
+//     res.render('shop-details', {shops})
+   
+// });
 shopDetails.get("/:id", function(req, res){
     let id: number = parseInt(req.params.id);
+    let errorRes: string = `No shop exists with ID ${id} `;
     for(let i = 0; i < shops.length; i++){
         if(shops[i].id === id){
-            res.json(shops[i]);
-            break;
-        }
+            let shop: Shop = shops[i];
+           res.render('shop-details', {shop});
+         }
+        //  if(shops[i].id !== id){
+        //      res.render('shop-details', {errorRes})
+        //  }
     }
 });
 
